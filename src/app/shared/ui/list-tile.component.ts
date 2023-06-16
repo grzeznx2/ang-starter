5bb9230e8f90d15d8c64356634c8e13eb0f427b2
grzeznx2 <grzeznx2@gmail.com>
2023-06-17 00:55:13 +0200

    Remove MatCard Module

--- src/app/shared/ui/list-tile.component.ts
+++ src/app/shared/ui/list-tile.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-list-tile',
   standalone: true,
-  imports: [CommonModule, MatCardModule],
+  imports: [CommonModule],
   template: ` <div class="p-4 shadow-md"><ng-content /></div> `,
   styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush,
