4250df2f9e14343c648da9056de1aa43380e17b5
grzeznx2 <grzeznx2@gmail.com>
2023-06-17 11:17:40 +0200

    Create and use abstract state service

--- src/app/features/ngo/data-access/ngos.state.service.ts
+++ src/app/features/ngo/data-access/ngos.state.service.ts
@@ -2,30 +2,23 @@ import { Injectable, signal } from '@angular/core';
 import { toObservable } from '@angular/core/rxjs-interop';
 import { NGO } from '../model/ngo.model';
 import { CallState } from 'src/app/core/call-state.enum';
+import { StateService } from 'src/app/core/state.service';
 
 export interface NGOsStateValue {
   list: NGO[];
   loadListCallState: CallState;
 }
 
+const initialState: NGOsStateValue = {
+  list: [],
+  loadListCallState: 'INITIAL',
+};
+
 @Injectable({
   providedIn: 'root',
 })
-export class NGOsStateService {
-  private $state = signal<NGOsStateValue>({
-    list: [],
-    loadListCallState: 'INITIAL',
-  });
-
-  get $value() {
-    return this.$state.asReadonly();
-  }
-
-  get value$() {
-    return toObservable(this.$state);
-  }
-
-  setState(value: Partial<NGOsStateValue>) {
-    this.$state.update(state => ({ ...state, ...value }));
+export class NGOsStateService extends StateService<NGOsStateValue> {
+  constructor() {
+    super(initialState);
   }
 }
